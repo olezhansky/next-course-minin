798f9382373fb31317d1c99b3ebf6b8ec8bc6c7d
olezhansky <fenix12895@gmail.com>
2022-01-16 17:39:25 +0200

    commit

---
+++ pages/posts.js
@@ -0,0 +1,60 @@
+import { useState, useEffect } from "react";
+import Head from "next/head";
+import { MainLayout } from "../components/MainLayout";
+import Link from "next/link";
+
+export default function Posts({ posts: serverPosts }) {
+  const [posts, setPosts] = useState(serverPosts);
+
+  useEffect(() => {
+    async function load() {
+      const response = await fetch("http://localhost:4200/posts");
+      const json = await response.json();
+      setPosts(json);
+    }
+
+    if (!serverPosts) {
+      load();
+    }
+  }, []);
+
+  if (!posts) {
+    return (
+      <MainLayout>
+        <p>Loading...</p>
+      </MainLayout>
+    );
+  }
+
+  return (
+    <MainLayout>
+      <Head>
+        <title>Posts Page</title>
+      </Head>
+      <h1>Posts</h1>
+      {/* <pre>{JSON.stringify(posts, null, 2)}</pre> */}
+      <ul>
+        {posts.map((post) => (
+          <li key={post.id}>
+            <Link href={`/post/[id]`} as={`/post/${post.id}`}>
+              <a>{post.title}</a>
+            </Link>
+          </li>
+        ))}
+      </ul>
+    </MainLayout>
+  );
+}
+
+Posts.getInitialProps = async ({ req }) => {
+  if (!req) {
+    return {
+      posts: null,
+    };
+  }
+  const response = await fetch("http://localhost:4200/posts");
+  const posts = await response.json();
+  return {
+    posts,
+  };
+};
